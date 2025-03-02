f9f4d06b4becab24f0fa5b5d17fa31fd438c1181
vishal patil <vishalgajananpatil4@gmail.com>
2025-03-02 19:36:11 +0530

    remaning web testcase automation

--- tests/SwagCartValidatios.spec.js
+++ tests/SwagCartValidatios.spec.js
@@ -1,12 +1,14 @@
 import {test, expect} from "@playwright/test"
 
-test('test', async ({ page }) => {
+test.beforeEach(async({page})=>{
     await page.goto('https://www.saucedemo.com/');
     await page.locator('[data-test="username"]').click();
     await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').click();
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
+})
+test('test', async ({ page }) => {
     await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
     await page.locator('[data-test="add-to-cart-sauce-labs-onesie"]').click();
 
@@ -26,6 +28,40 @@ test('test', async ({ page }) => {
     await expect(page.locator('[data-test="item-quantity"]').nth(1)).toContainText("1");
     await expect(page.locator('[data-test="inventory-item-name"]').nth(1)).toContainText("Sauce Labs Onesie");
     await expect(page.locator('[data-test="inventory-item-price"]').nth(1)).toContainText("$7.99");
+});
+test("Remove Items from Cart",async ({page})=>{
+    await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-onesie"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.screenshot({path:"before_3_Items.png"});
 
+    await page.locator('[data-test="shopping-cart-link"]').click();
 
+    await page.locator('[data-test="remove-sauce-labs-bike-light"]').click();
+
+    await page.locator('[data-test="continue-shopping"]').click();
+
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText("2");
+
+    await page.screenshot({path:'after_2_items.png'});
+
+});
+test("Complete Checkout Flow",async ({page})=>{
+        const item_total = '$17.98';
+        await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+        await page.locator('[data-test="add-to-cart-sauce-labs-onesie"]').click();
+        await page.locator('[data-test="shopping-cart-link"]').click();
+        await page.locator('[data-test="checkout"]').click();
+        await page.locator('[data-test="firstName"]').click();
+        await page.locator('[data-test="firstName"]').fill('vishal');
+        await page.locator('[data-test="firstName"]').press('Tab');
+        await page.locator('[data-test="lastName"]').fill('Patil');
+        await page.locator('[data-test="lastName"]').press('Tab');
+        await page.locator('[data-test="postalCode"]').fill('411225');
+        await page.locator('[data-test="continue"]').click();
+        await expect(page.locator('[data-test="subtotal-label"]')).toContainText(`${item_total}`);
+        await page.locator('[data-test="finish"]').click();
+
+        await expect(page.locator('[data-test="complete-header"]')).toContainText("Thank you for your order!");
+        await page.screenshot({path: "order_confirmation.png"});
 });
